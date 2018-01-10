5a1390f448c9de40335763d8269285b0e61fb927
Dibbo <DeflatedPickle@gmail.com>
2018-01-10 17:44:14 +0000

    Updated init

---
+++ vali/__init__.py
@@ -0,0 +1,13 @@
+from .vali import validate, ValidationError
+
+__title__ = "vali"
+
+__author__ = "llamicron"
+__copyright__ = "Copyright (c) 2018 llamicron"
+__credits__ = ["llamicron", "DeflatedPickle/Dibbo"]
+
+__license__ = ""
+__version__ = "1.0.0"
+__maintainer__ = "llamicron"
+__email__ = "llamicron@gmail.com"
+__status__ = "Development"
